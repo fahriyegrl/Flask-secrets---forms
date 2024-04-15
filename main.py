c33b1ae128deaeaccb71c242a37bdf48c5544c3a
fahriyegrl <115518015+fahriyegrl@users.noreply.github.com>
2024-04-15 13:28:46 -0400

    Update main.py

--- main.py
+++ main.py
@@ -3,18 +3,7 @@ from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField
 from wtforms.validators import DataRequired
 from flask_bootstrap import Bootstrap5
-'''
-Red underlines? Install the required packages first: 
-Open the Terminal in PyCharm (bottom left). 
 
-On Windows type:
-python -m pip install -r requirements.txt
-
-On MacOS type:
-pip3 install -r requirements.txt
-
-This will install the packages from requirements.txt for this project.
-'''
 class LoginForm(FlaskForm):
     email = StringField(label='Email', validators=[DataRequired()])
     password = PasswordField(label='Password', validators=[DataRequired()])
